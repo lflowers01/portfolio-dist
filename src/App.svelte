04ecef9a23ffd7c61d75d1bb3d4001b5a31e0e75
zenith <92184256+zenithdeveloper@users.noreply.github.com>
2023-01-18 21:01:28 -0700

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import About from "./lib/About.svelte";
+  import Navagation from "./lib/Navagation.svelte";
+</script>
+
+<main>
+  <Navagation />
+  <About />
+</main>
---
+++ src/lib/About.svelte
@@ -0,0 +1,13 @@
+
+<div id="about">
+  <div id="hero">
+    <p class="mono">Hi, my name is</p>
+   <h1>Lucas Flowers,</h1>
+  <h2>I am a computer science student.</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque laborum corrupti inventore tempore eaque cum, molestiae consequatur vero aliquid est.</p>
+  <p class="btn">
+    <a href="#">Button</a>
+  </p>
+</div>
+  
+</div>
